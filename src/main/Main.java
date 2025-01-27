c6d5efa1d67d59ba49ec59cf71da05ed1d9c6043
Tom Ferguson <148654475+TF2113@users.noreply.github.com>
2025-01-27 14:56:51 +0000

    Add files via manual upload

---
+++ src/main/Main.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package main;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Boatswain");
+
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+
+		window.pack();
+
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+
+		gamePanel.startGameThread();
+	}
+
+}
